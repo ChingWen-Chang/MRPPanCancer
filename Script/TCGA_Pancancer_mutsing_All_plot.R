75b29bd15f71cbe4e3af382a7cc3bc9f7de95d96
ChingWen-Chang <79670258+ChingWen-Chang@users.noreply.github.com>
2021-02-27 22:23:10 -0500

    Update TCGA_Pancancer_mutsing_All_plot.R

--- Script/TCGA_Pancancer_mutsing_All_plot.R
+++ Script/TCGA_Pancancer_mutsing_All_plot.R
@@ -1,4 +1,4 @@
-setwd("/Users/changc11/Documents/mutation_sing/mut_files")
+setwd("/Users/changc11/Documents/MRPPanCancer/RESULTS")
 
 Pan_cancer_TP53_mutation_group_all<- read.delim("/Users/changc11/Documents/Pan_cancer_mutation2/Mutation_TP53/Pan_cancer_TP53_mutation_group_all.txt",header = T)
 Pan_cancer_WT_Sing_Patient_100<- read.delim("/Users/changc11/Documents/mutation_sing/mut_files/Pan_cancer_WT_Sing_Patient_100.txt",header = T)
@@ -32,7 +32,6 @@ Pan_cancer_Sing_all_sum<-Pan_cancer_Sing_all %>%
   summarise_all(tibble::lst(mean))
 #write.table(Pan_cancer_Sing_all_sum,file="Pan_cancer_Sing_all_sum.txt",quote = F,sep = "\t")
 
-
 all.colors1 <-c("#67001f","#980043", "#ce1256", "#e7298a", "#df65b0", "#c994c7", "#d4b9da", "#e7e1ef",
                 "#ffffcc", "#f7fbff", "#deebf7", "#c6dbef", "#9ecae1", "#4292c6", "#2171b5", "#08519c", "#08306b",
                 "#bd0026", "#e31a1c", "#fc4e2a", "#fd8d3c", "#feb24c", "#fed976", "#ffeda0", "#ccece6", "#99d8c9",
@@ -54,9 +53,9 @@ Pan_cancer_Sing_all_sum_t_1$Group<-factor(Pan_cancer_Sing_all_sum_t_1$Group, lev
 # Barplot
 bp<- ggplot(Pan_cancer_Sing_all_sum_t_1, aes(x=Group, y=Signature, fill=group_new))+
   geom_bar(width = 1, stat = "identity")
-bp+ scale_fill_manual(values=all.colors1)
-pdf(file="Pan_cancer_All_plotSignatures1.pdf",width=6, height=4,compress=TRUE,useDingbats=F)
-bp+ scale_fill_manual(values=all.colors1)
+bp+ scale_fill_manual(values=all.colors1)+ theme(axis.text.x = element_text( size=8, angle=90))
+pdf(file="Pan_cancer_All_plotSignatures1.pdf",width=5, height=6,compress=TRUE,useDingbats=F)
+bp+ scale_fill_manual(values=all.colors1)+xlab("Mutation Type Probability (%)")+ theme(axis.text.x = element_text( size=10, angle=90))+scale_x_discrete(labels = c("WT", "P53-mut", "HMD", "P53-mut/HMD"))
 dev.off()
 
 
@@ -98,58 +97,35 @@ mydf_Group_4_P53_MRP_del_P53$P53_MRP_del_P53_ratio_log<-log2(mydf_Group_4_P53_MR
 mydf_Group_4_foldchange<-merge(mydf_Group_4_P53_WT, mydf_Group_4_MRP_del_WT, by="group_new")
 mydf_Group_4_foldchange<-merge(mydf_Group_4_foldchange, mydf_Group_4_P53_MRP_del_WT, by="group_new")
 mydf_Group_4_foldchange<-merge(mydf_Group_4_foldchange, mydf_Group_4_P53_MRP_del_P53, by="group_new")
-mydf_Group_4_foldchange$group_new <- factor(mydf_Group_4_foldchange$group_new, levels = all.sigs1)
+
+all.colors <-c("#00441b","#006d2c","#238b45","#41ae76","#66c2a4","#99d8c9","#ccece6","#ffeda0","#fed976","#feb24c","#fd8d3c","#fc4e2a","#e31a1c","#bd0026","#08306b","#08519c","#2171b5","#4292c6","#9ecae1","#c6dbef","#deebf7","#f7fbff","#ffffcc","#e7e1ef","#d4b9da","#c994c7","#df65b0","#e7298a","#ce1256","#980043","#67001f")
+all.sigs <-c("Signature.14","Signature.28","Signature.23","Signature.12","Signature.27","Signature.30","Signature.5","Signature.16","Signature.18","Signature.unknown","Signature.17","Signature.19","Signature.8","Signature.25","Signature.10","Signature.6","Signature.20","Signature.15","Signature.21","Signature.26","Signature.1","Signature.9","Signature.3","Signature.22","Signature.2","Signature.29","Signature.24","Signature.13","Signature.4","Signature.11","Signature.7")
+mydf_Group_4_foldchange$group_new <- factor(mydf_Group_4_foldchange$group_new, levels = all.sigs)
 
 
-pdf(file="Pan_cancer_All_plotSignatures_foldchange_P53_MRP_del_WT.pdf",width=5, height=3,compress=TRUE,useDingbats=F)
-ggplot(mydf_Group_4_foldchange, aes(group_new, P53_MRP_del_WT_ratio_log, fill=ifelse(P53_MRP_del_WT_ratio_log>0,"P53_MRP_del","WT"))) +
-  geom_bar(stat="identity") +
-  scale_fill_manual(values=c("red", "blue"), name="WT or P53_MRP_del") +
-  scale_y_continuous(labels = function(y) y )+ theme(axis.text.x = element_text(face="bold", color="black", size=8, angle=45))
+pdf(file="Pan_cancer_All_plotSignatures_foldchange_P53_MRP_del_P53.pdf",width=3, height=5,compress=TRUE,useDingbats=F)
+ggplot(mydf_Group_4_foldchange, aes(x=-P53_MRP_del_P53_ratio_log, y=group_new, fill=group_new)) +
+  geom_bar(stat="identity", position="dodge") +
+  guides(fill=FALSE)+ scale_fill_manual(values=all.colors)+
+  xlab("Foldchange") + ylab("Signature")+ ggtitle("P53-mut/HMD v/s P53-mut")+theme_minimal()
 dev.off()
 
-pdf(file="Pan_cancer_All_plotSignatures_foldchange_P53_MRP_del_P53.pdf",width=5, height=3,compress=TRUE,useDingbats=F)
-ggplot(mydf_Group_4_foldchange, aes(group_new, P53_MRP_del_P53_ratio_log, fill=ifelse(P53_MRP_del_P53_ratio_log>0,"P53_MRP_del","P53"))) +
-  geom_bar(stat="identity") +
-  scale_fill_manual(values=c("red", "blue"), name="P53 or P53_MRP_del") +
-  scale_y_continuous(labels = function(y) y )+ theme(axis.text.x = element_text(face="bold", color="black", size=8, angle=45))
+pdf(file="Pan_cancer_All_plotSignatures_foldchange_MRP_del_WT.pdf",width=3, height=5,compress=TRUE,useDingbats=F)
+ggplot(mydf_Group_4_foldchange, aes(x=-MRP_del_WT_ratio_log, y=group_new, fill=group_new)) +
+  geom_bar(stat="identity", position="dodge") +
+  guides(fill=FALSE)+ scale_fill_manual(values=all.colors)+
+  xlab("Foldchange") + ylab("Signature")+ ggtitle("HMD v/s WT")+theme_minimal()
 dev.off()
 
-pdf(file="Pan_cancer_All_plotSignatures_foldchange_MRP_del_WT.pdf",width=5, height=3,compress=TRUE,useDingbats=F)
-ggplot(mydf_Group_4_foldchange, aes(group_new, MRP_del_WT_ratio_log, fill=ifelse(MRP_del_WT_ratio_log>0,"MRP_del","WT"))) +
-  geom_bar(stat="identity") +
-  scale_fill_manual(values=c("red", "blue"), name="WT or MRP_del") +
-  scale_y_continuous(labels = function(y)y )+ theme(axis.text.x = element_text(face="bold", color="black", size=8, angle=45))
-dev.off()
-
-pdf(file="Pan_cancer_All_plotSignatures_foldchange_P53_WT.pdf",width=5, height=3,compress=TRUE,useDingbats=F)
-ggplot(mydf_Group_4_foldchange, aes(group_new, P53_WT_ratio_log, fill=ifelse(P53_WT_ratio_log>0,"P53","WT"))) +
-  geom_bar(stat="identity") +
-  scale_fill_manual(values=c("red", "blue"), name="WT or P53") +
-  scale_y_continuous(labels = function(y) y )+ theme(axis.text.x = element_text(face="bold", color="black", size=8, angle=45))
-dev.off()
-
-
-
+####################################
 library(dplyr)
 library(ggpubr)
 library(ggplot2)
 Pan_cancer_Sing_t<- read.delim("/Users/changc11/Documents/mutation_sing/mut_files/Pan_cancer_Sing_t.txt",header = T)
-# Box plot facetted by "dose"
-p <- ggboxplot(Pan_cancer_Sing_t, x = "group_new", y = "Signature",
-               color = "group_new", palette = "jco",
-               facet.by = "Signature_Type", short.panel.labs = FALSE)
-# Use only p.format as label. Remove method name.
-p + stat_compare_means(method = "anova", label.y = 1.2)+      # Add global p-value
-  stat_compare_means(label = "p.signif", method = "t.test",
-                     ref.group = ".all.", data =Pan_cancer_Sing_t)+ theme(axis.text.x = element_text(face="bold", color="black", size=8, angle=45))           
 Pan_cancer_Sing_p_Wilcoxon<-compare_means(Signature~group_new, data=Pan_cancer_Sing_t, group.by = "Signature_Type")
 Pan_cancer_Sing_p<-compare_means(Signature~group_new, data=Pan_cancer_Sing_t, group.by = "Signature_Type",method = "t.test")
 write.table(Pan_cancer_Sing_p,file="Pan_cancer_Sing_p.txt",quote = F,sep = "\t")
 write.table(Pan_cancer_Sing_p_Wilcoxon,file="Pan_cancer_Sing_p_Wilcoxon.txt",quote = F,sep = "\t")
-
-
-
 ##############################
 library(pheatmap)
 library(ComplexHeatmap)
@@ -159,11 +135,16 @@ library(RColorBrewer)
 
 mut.sig_p<- read.delim("/Users/changc11/Documents/mutation_sing/mut.sig_p.txt",header = T)
 mut.sig_p <- mut.sig_p[,-1]
-breaks = c(0, 0.001, 0.05, 0.06, 1)
+breaks = c(0, 0.001, 0.01, 0.05, 0.06)
 colors = c("red", "#fb6a4a", "#ffffb2", "#bdd7e7")
 p2<-pheatmap(mut.sig_p, color = colors, 
              show_rownames=T,
+             labels_col = c("P53-mut/HMD \n v/s P53-mut", "HMD v/s WT"),angle_col = "90",
              show_colnames =T, breaks=breaks, cluster_rows=F, cluster_cols=F)
-pdf(file="Pan_cancer_mut_sig_heatmapcrp.pdf",width=3, height=6)
+
+Heatmap(mut.sig_p, name = "ht2", 
+        col = circlize::colorRamp2(c(-2, 0, 2), c("green", "white", "red")),
+        column_names_gp = gpar(fontsize = 9))
+pdf(file="Pan_cancer_mut_sig_heatmapcrp.pdf",width=1, height=6)
 p2
 dev.off()
